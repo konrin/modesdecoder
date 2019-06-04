ba6f3cfbd5b6b522574eb631991d6a780db11d1f
Ilia Choly <icholy@accipiterradar.com>
2019-06-04 12:33:26 -0400

    update bin

--- bin.go
+++ bin.go
@@ -6,21 +6,17 @@ import (
 	"strings"
 )
 
-type Bin struct {
+type Bits struct {
 	bits []uint8
 	err  error
 }
 
-func (b *Bin) Len() int {
-	return len(b.bits)
+func NewBits(bits []uint8) *Bits {
+	return &Bits{bits: bits}
 }
 
-func (b *Bin) Err() error {
-	return b.err
-}
-
-func ParseHex(hex string) (*Bin, error) {
-	var bin Bin
+func ParseHex(hex string) (*Bits, error) {
+	var bin Bits
 	for _, r := range strings.ToLower(hex) {
 		c, ok := hexToBinLookup[r]
 		if !ok {
@@ -31,29 +27,49 @@ func ParseHex(hex string) (*Bin, error) {
 	return &bin, nil
 }
 
-func (b *Bin) Bits(from, to int) []uint8 {
-	if b.err != nil {
-		return nil
-	}
-	if from < 0 || to >= len(b.bits) {
-		b.err = fmt.Errorf("bits out of range (from=%d, to=%d, len=%d) ", from, to, len(b.bits))
-		return nil
-	}
-	return b.bits[from:to]
+func (b *Bits) Len() int {
+	return len(b.bits)
 }
 
-func (b *Bin) String(from, to int) string {
+func (b *Bits) Err() error {
+	return b.err
+}
+
+func (b *Bits) Copy() *Bits {
+	bits := make([]uint8, len(b.bits))
+	copy(bits, b.bits)
+	return &Bits{bits: bits}
+}
+
+func (b *Bits) Raw() []uint8 { return b.bits }
+
+func (b *Bits) At(i int) uint8 {
+	if b.err != nil {
+		return 0
+	}
+	if i < 0 || i >= len(b.bits) {
+		b.err = fmt.Errorf("bits out of range (i=%d, len=%d) ", i, len(b.bits))
+		return 0
+	}
+	return b.bits[i]
+}
+
+func (b *Bits) Bool(i int) bool {
+	return b.At(i) == 1
+}
+
+func (b *Bits) String(from, to int) string {
 	if b.err != nil {
 		return ""
 	}
 	s := ""
-	for _, x := range b.Bits(from, to) {
+	for _, x := range b.slice(from, to) {
 		s += strconv.Itoa(int(x))
 	}
 	return s
 }
 
-func (b *Bin) Int64(from, to int) int64 {
+func (b *Bits) Int64(from, to int) int64 {
 	s := b.String(from, to)
 	if b.err != nil {
 		return 0
@@ -64,4 +80,24 @@ func (b *Bin) Int64(from, to int) int64 {
 		return 0
 	}
 	return x
+}
+
+func (b *Bits) Uint(from, to int) uint {
+	return uint(b.Int64(from, to))
+}
+
+func (b *Bits) Slice(from, to int) *Bits {
+	bits := b.slice(from, to)
+	return &Bits{bits: bits, err: b.err}
+}
+
+func (b *Bits) slice(from, to int) []uint8 {
+	if b.err != nil {
+		return nil
+	}
+	if from < 0 || to >= len(b.bits) {
+		b.err = fmt.Errorf("bits out of range (from=%d, to=%d, len=%d) ", from, to, len(b.bits))
+		return nil
+	}
+	return b.bits[from:to]
 }
